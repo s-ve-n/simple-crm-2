3c10e29d055f7bedbd7bd06d027a814d137e0fde
plus <sven.radelof@gmail.com>
2022-05-23 12:07:04 +0000

    update

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,21 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import { Firestore, collectionData, collection } from '@angular/fire/firestore';
+import {
+  Firestore,
+  collectionData,
+  collection,
+  getDocs,
+} from '@angular/fire/firestore';
 import { MatDialog } from '@angular/material/dialog';
 import { Observable } from 'rxjs';
 import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 import { User } from '../model/user.class';
 
 export interface Users {
-  name: string;
-  position: number;
-  email: string;
+  firstName: string;
+  lastName: string;
+  birthDate: string;
   city: string;
 }
-
-// prettier-ignore
-const USER_DATA: Users[] = [
-  { position: 1, name: 'Hydrogen', email: 'test', city: 'city' }
-];
 
 @Component({
   selector: 'app-user',
@@ -26,23 +26,32 @@ export class UserComponent implements OnInit {
   user = new User();
   users$: Observable<any>;
   users: Array<any>;
+  USER_DATA: Users[] = [
+    { firstName: 'plus', lastName: 'minus', birthDate: '123', city: 'city' },
+  ];
 
   constructor(public dialog: MatDialog, private db: Firestore) {
     const coll = collection(db, 'users');
     this.users$ = collectionData(coll);
 
     this.users$.subscribe((newUsers) => {
-      console.log('new update');
+      console.log('new update', this.users);
+
       this.users = newUsers;
     });
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    // const querySnapshot = getDocs(collection(this.db, 'users'));
+    // querySnapshot.forEach((doc) => {
+    //   console.log(doc.id, ' => ', doc.data());
+    // });
+  }
 
   openDialog() {
     this.dialog.open(DialogAddUserComponent);
   }
 
-  displayedColumns: string[] = ['position', 'name', 'email', 'city'];
-  dataSource = USER_DATA;
+  displayedColumns: string[] = ['firstName', 'lastName', 'birthDate', 'city'];
+  dataSource = this.USER_DATA;
 }
